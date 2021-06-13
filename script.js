178a857cd6f21866a0da29738d2e21abcdd3c42e
Umarjon] <77933357+Um4rj0n@users.noreply.github.com>
2021-06-13 13:04:04 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 let t1, t2;
 let start = false;
+let Goo = document.querySelector('.start')
+let Kettik = document.querySelector('.pedals .treadleK')
+let Tormoz = document.querySelector('.pedals .treadleT')
 const engine = new Audio('./audio/engine.mp3');
 const audio = new Audio('./audio/car.mp3');
+const tormozp = new Audio('./audio/qGGGG.mp3');
 engine.loop = true;
 engine.volume = 0.3;
 
-document.querySelector('.start').addEventListener('click', function () {
-    if (start === false) {
-        start = true;
-        this.innerHTML = 'stop';
-        engine.play();
-        document.querySelector('.treadle').addEventListener('click', pushTreadle); // событие на педаль;
-        document.querySelector('.progress-line').style.width = '100px';
-    }
-    else {
-        // глушим
-        start = false;
+Goo.addEventListener('click', function () {
+
+    this.innerHTML = 'stop';
+    engine.play();
+    Kettik.addEventListener('mouseover', pushTreadle
+    ); // событие на педаль;
+    document.querySelector('.progress-line').style.width = '100px';
+
+    Goo.addEventListener('click', function () {
+
         this.innerHTML = 'start';
         engine.pause();
-        document.querySelector('.treadle').removeEventListener('click', pushTreadle); // событие на педаль;
         document.querySelector('.progress-line').style.width = '0';
-        document.querySelector('.treadle').classList.remove('treadle-push');
-        t1 = clearTimeout(t1);
-        t2 = clearTimeout(t2);
-        audio.pause();
-        audio.currentTime = 0;
-    }
+
+    })
+    // // глушим
+    Kettik.removeEventListener('mouseout', stopTreadle); // событие на педаль;
+    Tormoz.addEventListener('mouseover', pushTreadleT
+    );
+    Tormoz.removeEventListener('mouseout', stopTreadleT); // событие на педаль;
 });
+
 
 function pushTreadle() {
     t1 = clearTimeout(t1);
@@ -39,11 +43,29 @@ function pushTreadle() {
 
 function stopTreadle() {
     t1 = setTimeout(() => {
-        document.querySelector('.treadle').classList.remove('treadle-push');
+        Kettik.classList.remove('treadle-push');
         document.querySelector('.progress-line').style.width = '100px';
     }, 1000);
     t2 = setTimeout(() => {
         audio.pause();
         audio.currentTime = 0;
+    }, 1500);
+}
+function pushTreadleT() {
+    t1 = clearTimeout(t1);
+    t2 = clearTimeout(t2);
+    this.classList.add('treadle-push');
+    document.querySelector('.progress-line').style.width = '50px';
+    tormozp.play();
+    stopTreadleT();
+}
+function stopTreadleT() {
+    t1 = setTimeout(() => {
+        Tormoz.classList.remove('treadle-push');
+        document.querySelector('.progress-line').style.width = '50px';
+    }, 1000);
+    t2 = setTimeout(() => {
+        tormozp.pause();
+        tormozp.currentTime = 0;
     }, 1500);
 }
